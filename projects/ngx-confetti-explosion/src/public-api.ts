efd3cc8f567139db7cd0f9bf8640be0072f05e58
Chellappan <vrchellappan@gmail.com>
2023-07-29 22:13:38 +0530

    delete removed file path from index.ts

--- projects/ngx-confetti-explosion/src/public-api.ts
+++ projects/ngx-confetti-explosion/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-confetti-explosion
  */
 
-export * from './lib/ngx-confetti-explosion.service';
 export * from './lib/ngx-confetti-explosion.component';
 export * from './lib/ngx-confetti-explosion.module';

fc94ad9008eb4cc18af635f8ee3cc9387e62b0a0
Chellappan <vrchellappan@gmail.com>
2023-11-11 12:25:08 +0530

    refactor: update control flow syntax (#2)

--- projects/ngx-confetti-explosion/src/lib/ngx-confetti-explosion.module.ts
+++ projects/ngx-confetti-explosion/src/lib/ngx-confetti-explosion.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { NgxConfettiExplosionComponent } from './ngx-confetti-explosion.component';
-import { NgFor } from '@angular/common';
-
-
 
 @NgModule({
   declarations: [
     NgxConfettiExplosionComponent
-  ],
-  imports: [
-    NgFor
   ],
   exports: [
     NgxConfettiExplosionComponent

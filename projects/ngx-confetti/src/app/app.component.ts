15898698f17cb9e491031cb41aa1c60536ae2005
ChellappanRajan <vrchellappan@gmail.com>
2024-05-11 19:28:27 +0530

    convert sample app style processor from sass to scss

--- projects/ngx-confetti/src/app/app.component.ts
+++ projects/ngx-confetti/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxConfettiExplosionComponent } from 'ngx-confetti-explosion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgxConfettiExplosionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ngx-confetti';
+  show = false;
+  onShow(){
+    this.show = true;
+  }
+  onExplosionDone(){
+    this.show = false;
+  }
 }
